787fd6f20f3a9f28134df56303554f8806033201
Baptiste Entat <bapt84@gmail.com>
2022-01-09 18:57:45 +0100

    add plusOuMoins actv

---
+++ Act/plusOuMoins_COOLGAME/plusOuMoins_COOLGAME.cpp
@@ -0,0 +1,44 @@
+﻿// plusOuMoins_COOLGAME.cpp : définit le point d'entrée de l'application.
+//
+
+#include "plusOuMoins_COOLGAME.h"
+using namespace std;
+
+int get_userInput();
+
+int main()
+{
+
+	//generate one random number, betwen 0 and 100
+	srand((unsigned)time(0));
+	int randomInt = rand()%100;
+
+	/*   To-Do list
+	* demande est stocke l'entrée utilisateur à chaque tour dans le while,
+	* compare dans les if/else l'input au nb mystere,
+	* -> si c'est le bon, il faut sortir de la boucle,
+	* -> sinon faire en fonction
+	*/
+
+	while (true) //true ==> never stop, but break; instruction can do it !
+	{
+		if (true)
+		{}
+		else if (false)
+		{}
+		else
+		{}
+	}
+
+	cout << "\nYou Win !\n   the secret number was: " << randomInt << "\n";
+
+
+	return 0;
+}
+
+int get_userInput()
+{
+	string buffInput{};
+	cin >> buffInput;
+	return stoi(buffInput);
+}
---
+++ Act/plusOuMoins_COOLGAME/plusOuMoins_COOLGAME.h
@@ -0,0 +1,10 @@
+﻿// plusOuMoins_COOLGAME.h : fichier Include pour les fichiers Include système standard,
+// ou les fichiers Include spécifiques aux projets.
+
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+
+// TODO: Référencez ici les en-têtes supplémentaires nécessaires à votre programme.
